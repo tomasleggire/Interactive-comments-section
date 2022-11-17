3260ecaef960889d8a587ada171050a4afd9619b
Tomás Leggire <tomasleggire@hotmail.com>
2022-11-17 20:44:52 -0300

    Contador maquetado

--- src/App.js
+++ src/App.js
@@ -16,35 +16,46 @@ function App() {
       img: './IMAGES/image-amyrobson.png',
       replies: [], 
     },
-    {
-      user: 'maxblagun',
-      date: '2 weeks ago',
-      contador: 5,
-      msg: `Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!`,
-      img: './IMAGES/image-maxblagun.png',
-      replies: [
-        {
-          user: 'ramsesmiron',
-          date: '1 week ago',
-          contador: 4,
-          msg: `If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.`,
-          img: './IMAGES/image-ramsesmiron.png',
-        },
-        {
-          user: 'juliusomo',
-          date: '2 days ago',
-          contador: 2,
-          msg: `I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.`,
-          img: './IMAGES/image-juliusomo.png',
-        }
-      ], 
-    }
+    // {
+    //   user: 'maxblagun',
+    //   date: '2 weeks ago',
+    //   contador: 5,
+    //   msg: `Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!`,
+    //   img: './IMAGES/image-maxblagun.png',
+    //   replies: [
+    //     {
+    //       user: 'ramsesmiron',
+    //       date: '1 week ago',
+    //       contador: 4,
+    //       msg: `If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.`,
+    //       img: './IMAGES/image-ramsesmiron.png',
+    //     },
+    //     {
+    //       user: 'juliusomo',
+    //       date: '2 days ago',
+    //       contador: 2,
+    //       msg: `I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.`,
+    //       img: './IMAGES/image-juliusomo.png',
+    //     }
+    //   ], 
+    // }
   ])
 
   return (
     <div className='main-app'>
-      <p>HOLA</p>
-      <Comment />
+      {state.map(function(user) {
+        return (
+          <Comment
+            YOU={YOU}
+            user={user.user}
+            date={user.date}
+            contador={user.contador}
+            msg={user.msg}
+            img={user.img}
+            replies={user.replies}
+          />
+        )
+      })}
     </div>
   );
 }
--- src/COMPONENTS/Comment.js
+++ src/COMPONENTS/Comment.js
@@ -1,10 +1,22 @@
 import React, {useState} from "react";
 import '../CSS/Comment.css';
+import {FaPlus} from "react-icons/fa";
+import {FaMinus} from "react-icons/fa";
 
-export default function Comment() {
+export default function Comment({YOU, user, date, contador, msg, img, replies}) {
 
 
     return (
-        <p className="p">HOLA HOLA</p>
+        <div className="main-comment">
+            <div className="main-comment-contador">
+                <div className="contador">
+                    <button className="btn-contador"><FaPlus /></button>
+                    <span className="span-contador">{contador}</span>
+                    <button className="btn-contador"><FaMinus /></button>
+                </div>
+            </div>
+            <div className="main-comment-info">
+            </div>
+        </div>
     )
 }
